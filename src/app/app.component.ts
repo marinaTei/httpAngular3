a31c1d8a772c587232fb8dc9e5be8a3971bc6fe1
marinaTei <37223833+marinaTei@users.noreply.github.com>
2018-08-14 14:51:40 +0300

    v3 pagination, search, filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
 import {Component} from '@angular/core';
+import {NgxPaginationModule} from 'node_modules/ngx-pagination';
 
 import {GlobalService} from './services/global.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
     title = 'Users App';
 
     constructor(private global: GlobalService) {
     }
-
+    search(value: any) {
+        console.log(value);
+        this.global.search = value;
+    }
     changeValue() {
+
         if (this.global.show === true) {
             alert('Close without deleting user?');
         }
         this.global.value = 'post';
         this.global.show = !this.global.show;
+        this.global.user.active = null;
     }
 }
